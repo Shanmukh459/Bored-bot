b7608078f04a980d41682b3910de677deff22493
Shanmukh459 <saikrrish459@gmail.com>
2024-01-15 16:55:53 -0800

    Bored bot project code

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+Challenge: 
+
+- Make the styling more exciting once an activity idea comes 
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com, 
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more 
+        excitement to the page
+*/
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
+document.getElementById("get-activity").addEventListener("click", function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("title").textContent = "🎇Let's do this!🥳"
+      document.getElementById("activity").textContent = `🎉${data.activity}🎉`
+    })
+    confetti()
+})
